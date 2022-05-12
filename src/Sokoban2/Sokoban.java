c5215df120c0c8992680e72f3415d041bfa2071a
hera1234567 <99333374+hera1234567@users.noreply.github.com>
2022-05-12 10:59:30 +0200

    javadoc, tog bort gamla versionen

--- src/Sokoban2/Sokoban.java
+++ src/Sokoban2/Sokoban.java
@@ -14,7 +14,6 @@ public class Sokoban implements Controller{
     //TODO FIXA SÅ ATT DET GÅR MED MOUSPAD OCKSÅ
     private InputMethod m = new KeyPad(Frame.getFrame());
 
-
     //endregion
 
 
@@ -23,7 +22,6 @@ public class Sokoban implements Controller{
         current = setLevel(lvlCounter);
         winFlag = false;
         loseFlag = false;
-        // m = new KeyPad(Frame.getFrame());
         m.setController(this);
     }
 
--- src/Sokoban2/Test.java
+++ src/Sokoban2/Test.java
@@ -2,21 +2,19 @@ package Sokoban2;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.swing.*;
-import java.awt.*;
 import java.io.IOException;
 
 public class Test {
 
     public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
 
-      //  PositionObserver obs = new PositionObserver();
+        //PositionObserver obs = new PositionObserver();
         GraphicObserver sok = new GraphicObserver();
-      //  SoundObserver sound = new SoundObserver();
+        SoundObserver sound = new SoundObserver();
         Sokoban s = new Sokoban();
         s.setInputMethod(new MousePad(Frame.getFrame()));
         s.addObserver(sok);
-       // s.addObserver(obs);
-       // s.addObserver(sound);
+        //s.addObserver(obs);
+        s.addObserver(sound);
     }
 }
