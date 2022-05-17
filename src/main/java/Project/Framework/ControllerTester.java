a6146226adf7a1a3b6b40d27050c6d53318472e7
hera1234567 <99333374+hera1234567@users.noreply.github.com>
2022-05-17 16:09:31 +0200

    ControllerTester

---
+++ src/main/java/Project/Framework/ControllerTester.java
@@ -0,0 +1,80 @@
+package Project.Framework;
+
+import Project.Sokoban.GraphicObserver;
+import Project.Sokoban.Sokoban;
+import Project.Sokoban.SoundObserver;
+
+public class ControllerTester implements Controller{
+
+   public ControllerTester(){
+
+   }
+
+    @Override
+    public void walkUp() {
+        System.out.println("UP");
+    }
+
+    @Override
+    public void walkDown() {
+        System.out.println("DOWN");
+    }
+
+    @Override
+    public void walkRight() {
+        System.out.println("RIGHT");
+    }
+
+    @Override
+    public void walkLeft() {
+        System.out.println("LEFT");
+    }
+
+    @Override
+    public void clickedPosition(int xMouse, int yMouse, int frameWidth, int frameHeight) {
+        System.out.println("MOUSE");
+    }
+
+    @Override
+    public void nextLevel() {
+        System.out.println("NEXT");
+    }
+
+    @Override
+    public void restartLevel() {
+        System.out.println("RESTART LEVEL");
+    }
+
+    @Override
+    public void restartGame() {
+        System.out.println("RESTART GAME");
+    }
+
+}
+
+class Test{
+    public static void main(String[] args){
+
+        //Testing keypad
+        GraphicObserver sok = new GraphicObserver();
+        SoundObserver sound = new SoundObserver();
+        Sokoban s = new Sokoban();
+        s.setInputMethod(new KeyPad(Frame.getFrame()));
+        s.addObserver(sok);
+        s.addObserver(sound);
+        s.setController(new ControllerTester());
+
+
+
+        //Testing mousepad
+       /* GraphicObserver sok = new GraphicObserver();
+        SoundObserver sound = new SoundObserver();
+        Sokoban s = new Sokoban();
+        s.setInputMethod(new MousePad(Frame.getFrame()));
+        s.addObserver(sok);
+        s.addObserver(sound);
+        s.setController(new ControllerTester());
+
+        */
+    }
+}
